9b35215742820083083f3836dde5dc3f0e239454
Anton Ambrose <antoncda@gmail.com>
2021-07-19 15:56:29 +0100

    added dropzone

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import Titlebar from './components/Titlebar'
+import React, { useEffect, useState } from 'react';
+import { invoke } from '@tauri-apps/api/tauri';
+import { open, Command } from '@tauri-apps/api/shell';
+import Titlebar from './components/Titlebar';
+import DropZone from './DropZone';
 import './App.css';
+import { readTextFile } from '@tauri-apps/api/fs';
 
 function App() {
-  return (
-    <div className="App">
-      <Titlebar />
-    </div>
-  );
+	const [text, setText] = useState('');
+	const handleSubmit = async () => {
+		invoke('get_args').then((message) => console.log(message));
+		//await open('test.txt', 'notepad');
+		//const output = await open('chdir');
+		//const output = await new Command('pwd').execute();
+		// const output = await readTextFile('./notes.txt');
+		// setText(output);
+		// console.log('output', output);
+	};
+	return (
+		<div className='App'>
+			<Titlebar />
+			<div className='frame'>
+				<h2>Hello World</h2>
+				<button onClick={handleSubmit}>Click</button>
+				{/* <h3>{text}</h3> */}
+			</div>
+			<DropZone />
+		</div>
+	);
 }
-
 export default App;
